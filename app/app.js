74b08d03e311b6b16a02cc08360cfd3ba3196c20
myciek00 <m_rychel@op.pl>
2017-10-28 16:33:43 +0200

    testing travis and heroku

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
-  res.send('Hello Heroku');
+  res.send('Hello Heroku - travis test !');
 });
 
 app.listen(app.get('port'), function() {
