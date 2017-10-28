39fef1f37cdea0c06b62c4dbe7e2054bed6af184
myciek00 <m_rychel@op.pl>
2017-10-28 17:18:49 +0200

    testing skipp [skip ci]

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
-  res.send('Hello Heroku - travis test ! part 2');
+  res.send('Hello Heroku - travis test ! part 2 - this is not going to be deployed [ci skip]');
 });
 
 app.listen(app.get('port'), function() {
