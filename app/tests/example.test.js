01b1a0b50cbb5c66ebaf98a21e2229747b3b5950
myciek00 <m_rychel@op.pl>
2017-11-04 11:28:15 +0100

    fighting tavis 3...

--- app/tests/example.test.js
+++ app/tests/example.test.js
@@ -1,4 +1,5 @@
 const expect = require('expect');
+const request = require('supertest');
 
 describe('example success test for travis build', () => {
 
@@ -8,6 +9,23 @@ describe('example success test for travis build', () => {
 
   it('I am not failing :) too', () => {
     expect(310).toBe(310);
+  });
+
+});
+
+const { app } = require('./../app');
+
+describe('GET /api/restaurants', () => {
+
+  it('should get all restaurants', done => {
+    request(app)
+      .get('/api/restaurants')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .expect(res => {
+        expect(res.body.rows).toBeTruthy();
+      })
+      .end(done);
   });
 
 });
